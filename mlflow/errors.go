ee1f18cf8c8f967fd0222ece7c01c6158ddc5ef1
Ram Dayan <ramdayan@gmail.com>
2024-06-26 14:31:08 +0300

    Handle MLflow errors (#4)

---
+++ mlflow/errors.go
@@ -0,0 +1,20 @@
+package mlflow
+
+const (
+	// ErrorResourceAlreadyExists indicates that a resource with the given name already exists.
+	ErrorResourceAlreadyExists = "RESOURCE_ALREADY_EXISTS"
+
+	// ErrorResourceDoesNotExist indicates that the requested resource does not exist.
+	ErrorResourceDoesNotExist = "RESOURCE_DOES_NOT_EXIST"
+)
+
+// Error represents an error returned by the MLflow API.
+type Error struct {
+	ErrorCode string `json:"error_code"`
+	Message   string `json:"message"`
+}
+
+// Error returns the error message.
+func (e *Error) Error() string {
+	return e.Message
+}
