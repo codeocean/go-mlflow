3b66378c6fddc0d5758740e0baa5db601f1867fc
Eyal Falk <eyal@codeocean.com>
2024-07-18 10:58:48 +0300

    reorder params

--- mlflow/models.go
+++ mlflow/models.go
@@ -37,7 +37,7 @@ type ModelVersionTag struct {
 	Value string `json:"value"`
 }
 
-func (s *ModelService) SetTag(ctx context.Context, name, key, value string, version int) error {
+func (s *ModelService) SetTag(ctx context.Context, name string, version int, key, value string) error {
 	opts := struct {
 		Name    string `json:"name,omitempty"`
 		Version string `json:"version,omitempty"`
